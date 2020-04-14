c5a2aa3578869ebe4ca3ba27bf06e40a0526b257
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-14 16:59:15 -0700

    recipereducer and other reducers skeleton

--- src/reducers/recipeReducer.js
+++ src/reducers/recipeReducer.js
@@ -1,8 +1,22 @@
 import * as types from '../constants/actionTypes';
 
 const recipeState = {
-    recipes: [], //recipes 
-    img: '',
-    //video: stretch feature
-    nutrition: '',
-  };
+  recipes: [], //recipes
+  img: '',
+  //video: stretch feature
+  nutrition: ''
+};
+
+const recipeReducer = (state = recipeState, action) => {
+  switch (action.type) {
+    case types.ADD_RECIPE:
+      return {
+        ...state,
+        recipes: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default recipeReducer;
