517a99237d907fae56f19e7f093246fe7cc1745a
Anthony Flores <anthony.flores2293@gmail.com>
2020-04-14 14:12:29 -0700

    fridge and landing containers

---
+++ src/containers/Fridge.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Fridge = (props) => {
+  return <h2>We are in Fridge</h2>;
+};
+
+export default Fridge;
---
+++ src/containers/Landing.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Landing = (props) => {
+  return <Button href="/fridge">Login</Button>;
+};
+
+export default Landing;
