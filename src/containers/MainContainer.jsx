8e2406ed618f046aa4390096d0738e35794f96a7
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-14 09:44:17 -0700

    mainContainer boilerplate

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,5 +1,25 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+const mapStateToProps = ({ user: { isLoggedIn } }) => ({
+    isLoggedIn
+});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+class MainContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="MainContainer">
+                <h2>Hello BaskinOS</h2>
+            </div>
+        );
+    }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
