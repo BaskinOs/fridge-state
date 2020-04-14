0a6c1aff882b437422e5fc5e135dd545fc3bbd2a
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-14 16:57:48 -0700

    readded button

--- src/containers/Landing.jsx
+++ src/containers/Landing.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const Landing = (props) => {
-  // return <Button href="/fridge">Login</Button>;
   return (
     <div id="landing">
-    <form id="login-form" method="GET" action="/auth/google">
-      <button className="loginBtn" id="loginBtn" type="submit">
-        Login with Google
-      </button>
-    </form>
-    <form id="logout-form" method="GET" action="/auth/logout">
-    <button className="loginBtn" id="loginBtn" type="submit">
-      Logout
-    </button>
-  </form>
-  </div>
-  )
+      <form id="login-form" method="GET" action="/auth/google">
+        <button className="loginBtn" id="loginBtn" type="submit">
+          Login with Google
+        </button>
+      </form>
+      <form id="logout-form" method="GET" action="/auth/logout">
+        <button className="loginBtn" id="loginBtn" type="submit">
+          Logout
+        </button>
+      </form>
+      <Button href="/fridge">Move to Fridge</Button>
+    </div>
+  );
 };
 
 export default Landing;
