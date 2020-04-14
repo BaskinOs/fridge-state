dd31d0c6b1970c5e2899b58e8fc9794e719aa7db
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-14 16:56:27 -0700

    ingredients action types (fetches)

---
+++ src/actions/ingredientActions.js
@@ -0,0 +1,42 @@
+// import actionType constants
+import axios from 'axios';
+import * as types from '../constants/actionTypes';
+
+export const postIngredient = () => (dispatch, getState) =>
+  axios
+    .post('/api/ingredients', {
+      user_id: getState().user.userId,
+      ingredient: getState().ingredient.messageInput
+    })
+    .then(({ data }) => {
+      if (!data.isLoggedIn) {
+        dispatch({
+          type: types.USER_LOGOUT,
+          payload: data
+        });
+      } else {
+        dispatch({
+          type: types.POST_INGREDIENT,
+          payload: data
+        });
+      }
+    });
+
+export const getIngredients = () => (dispatch, getState) =>
+  axios
+    .get('/api/ingredients', {
+      user_id: getState().user.userId
+    })
+    .then(({ data }) => {
+      if (!data.isLoggedIn) {
+        dispatch({
+          type: types.USER_LOGOUT,
+          payload: data
+        });
+      } else {
+        dispatch({
+          type: types.GET_INGREDIENTS,
+          payload: data
+        });
+      }
+    });
