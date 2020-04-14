c41bf03b538fed85a7f5a046f261057a13ad26cd
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-14 09:43:56 -0700

    added maincontainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import { hot } from "react-hot-loader";
-import styles from "./styles/styles.scss";
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+import styles from './styles/styles.scss';
 
-import MainContainer from "./containers/MainContainer";
+import MainContainer from './containers/MainContainer.jsx';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <MainContainer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h2>WHATS UP</h2>
+            </div>
+        );
+    }
 }
 
 export default hot(module)(App);
