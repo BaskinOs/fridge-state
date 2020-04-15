7c66b98de36402a71d76f6326399001a6d28e8d1
semtemp <55463525+semtemp@users.noreply.github.com>
2020-04-15 10:41:37 -0700

    added auth check

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const passport = require('passport');
 
+const userController = require('../controllers/userController');
+
 const authRouter = express.Router();
-
-// authRouter.get('/login', (req, res) => {
-//   const { email, password } = req.body
-//   return res.status(200).json({ success: true, data })
-// })
-
-// authRouter.post('/register', async (req, res) => {
-//   const { firstName, lastName, email, password } = req.body
-//   return res.status(200).json({ success: true, data: null })
-// })
 
 //auth logout
 authRouter.get('/logout', (req, res) => {
@@ -20,21 +12,33 @@ authRouter.get('/logout', (req, res) => {
   res.send('loggin out')
 })
 
+//checks if user is logged in
+const authCheck = (req, res, next) => {
+  if(!req.user) { //user not logged in
+    res.redirect('/');
+  }
+  else {
+    next();
+  }
+}
+
+//callback route for google redirect
+//pass code received from google to passport
+authRouter.get('/google/callback', passport.authenticate('google'), authCheck, (req, res) => {
+  //req.user has user info from cookie
+  //redirect logged in user to dashboard
+  res.locals.user = req.user;
+  res.redirect('http://localhost:8080/dashboard');
+});
+
+authRouter.get('/verify', userController.verify, (req, res) => {
+  res.status(200).json(res.locals);
+})
 
 //auth with google using passport
 authRouter.get('/google', passport.authenticate('google', {
-  scope: ['profile']
+    scope: ['profile']
 }));
 
-//callback route for google redirect
-authRouter.get('/google/callback', (res, req) => {
-  res.send('you reached callback URI');
-});
-
-// authRouter.get('/google/callback', 
-//   passport.authenticate('google', { failureRedirect: '/auth' }),
-//   function(req, res) {
-//     res.redirect('/');
-//   });
 
 module.exports= authRouter;
