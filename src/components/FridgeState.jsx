d90d951d40667f21f15f503a420e424460c67b01
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-15 13:51:16 -0700

    added ingredients state to fridgestate

--- src/components/FridgeState.jsx
+++ src/components/FridgeState.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => ({
+  ingredientsList: state.ingredient.ingredients
+});
 
 const FridgeState = (props) => {
+  //ingredients will populate here
+  console.log('fridgeState props', props.ingredientsList);
   return (
     <div className="fridgeState">
       <h2>Fridge State</h2>
@@ -8,4 +15,4 @@ const FridgeState = (props) => {
   );
 };
 
-export default FridgeState;
+export default connect(mapStateToProps)(FridgeState);
