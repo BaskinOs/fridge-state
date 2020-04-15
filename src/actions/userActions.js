7e31b744fb74fdf4e159c3948e034a188eda1ed9
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-14 22:12:24 -0700

    linter update

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,36 +1,36 @@
 // import actionType constants
-import axios from "axios";
-import * as types from "../constants/actionTypes";
+import axios from 'axios';
+import * as types from '../constants/actionTypes';
 
 export const verifyLogin = () => (dispatch) =>
   axios
-    .get("/login/verify")
+    .get('/login/verify')
     .then(({ data }) => {
       if (!data.isLoggedIn) {
         return dispatch({
           type: types.USER_LOGOUT,
-          payload: data,
+          payload: data
         });
       } else {
         return dispatch({
           type: types.USER_LOGIN,
-          payload: data,
+          payload: data
         });
       }
     })
     .catch((err) => console.log(err));
 
-export const getUserData = () => (dispatch) =>
-  axios.get("/api/user").then(({ data }) => {
+export const getUser = () => (dispatch) =>
+  axios.get('/api/user').then(({ data }) => {
     if (!data.isLoggedIn) {
       dispatch({
         type: types.USER_LOGOUT,
-        payload: data,
+        payload: data
       });
     } else {
       dispatch({
-        type: types.LOAD_USER,
-        payload: data.user,
+        type: types.GET_USER,
+        payload: data.user
       });
     }
   });
