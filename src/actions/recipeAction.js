a2466e5df0ca255a32f67b3ea95b489dc87797b0
Anthony Flores <anthony.flores2293@gmail.com>
2020-04-15 14:04:20 -0700

    added boilerplate for recipe Action

--- src/actions/recipeAction.js
+++ src/actions/recipeAction.js
@@ -1,2 +1,26 @@
 import axios from 'axios';
 import * as types from '../constants/actionTypes';
+
+export const getRecipes = () => (dispatch, getState) =>
+  axios
+    .get('/', {})
+    .then(({ data }) => {
+      console.log(data);
+      dispatch({
+        type: types.GET_RECIPES,
+        payload: data
+      });
+    })
+    .catch((err) => console.log(`ERR in getRecipes Action: ${err}`));
+
+export const getInstructions = () => (dispatch, getState) =>
+  axios
+    .get('/', {})
+    .then(({ data }) => {
+      console.log(data);
+      dispatch({
+        type: types.GET_INSTRUCTIONS,
+        payload: data
+      });
+    })
+    .catch((err) => console.log(`ERR in getInstructions Action: ${err}`));
