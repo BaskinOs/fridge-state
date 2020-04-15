cc969aa96ef7537f50297a2b7044dff40653c4d3
semtemp <55463525+semtemp@users.noreply.github.com>
2020-04-15 10:47:26 -0700

    commented logout, rendering based on login status

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import Wrapper from "./containers/Wrapper";
+import { bindActionCreators } from "redux";
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -10,22 +10,50 @@ import Ingredients from './Ingredients';
 import Recipes from './Recipes';
 import Instructions from './Instructions';
 import Dashboard from './Dashboard';
+import * as userActions from "../actions/userActions";
 
 const mapStateToProps = ({ user: { isLoggedIn } }) => ({
   isLoggedIn
 });
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => bindActionCreators(userActions, dispatch)
 
 class MainContainer extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    console.log('componentDidMount');
+    // this.props.verifyLogin();
+  }
+
+  componentWillUnmount() {
+    // clearInterval(this.interval);
+    console.log('componentWillUnmount');
+  }
+
   render() {
-    return (
+    const { isLoggedIn } = this.props;
+    console.log('isLoggedIn', isLoggedIn);
+    if (!isLoggedIn) {
+      return (
+        <BrowserRouter>
+        {/* <div className="MainContainer">Main Container</div> */}
+        <Route path="/" exact component={Landing} />
+        {/* <Route path="/fridge" render={() => <Fridge />} />
+        <Route path="/ingredients" render={() => <Ingredients />} />
+        <Route path="/recipes" render={() => <Recipes />} />
+        <Route path="/instructions" render={() => <Instructions />} />
+        <Route path="/dashboard" render={() => <Dashboard />} /> */}
+        <p>Please log in to use the app</p>
+      </BrowserRouter>
+      )
+    }
+    else return (
       <BrowserRouter>
         <div className="MainContainer">Main Container</div>
+        <p>Logged In!</p>
         <Route path="/" exact component={Landing} />
         <Route path="/fridge" render={() => <Fridge />} />
         <Route path="/ingredients" render={() => <Ingredients />} />
