25c7b94e5bb267bdad72a08517fc72b6ad9d4e11
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-14 22:12:00 -0700

    uncommented islogged in for testing, added updateIngred for input field

--- src/actions/ingredientActions.js
+++ src/actions/ingredientActions.js
@@ -6,20 +6,21 @@ export const postIngredient = () => (dispatch, getState) =>
   axios
     .post('/api/ingredients', {
       user_id: getState().user.userId,
-      ingredient: getState().ingredient.messageInput
+      ingredient: getState().ingredient.ingredientInput
     })
     .then(({ data }) => {
-      if (!data.isLoggedIn) {
-        dispatch({
-          type: types.USER_LOGOUT,
-          payload: data
-        });
-      } else {
-        dispatch({
-          type: types.POST_INGREDIENT,
-          payload: data
-        });
-      }
+      // if (!data.isLoggedIn) {
+      //   dispatch({
+      //     type: types.USER_LOGOUT,
+      //     payload: data
+      //   });
+      // } else {
+      console.log(data);
+      dispatch({
+        type: types.POST_INGREDIENT,
+        payload: data
+      });
+      // }
     });
 
 export const getIngredients = () => (dispatch, getState) =>
@@ -60,3 +61,8 @@ export const deleteIngredient = () => (dispatch, getState) =>
         });
       }
     });
+
+export const updateIngredient = (event) => ({
+  type: types.UPDATE_INGREDIENT,
+  payload: event.target.value
+});
