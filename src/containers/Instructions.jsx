9f278821459367b51872b1a49e09905b14df8529
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-16 02:08:17 -0700

    summary (instructions) rendering

--- src/containers/Instructions.jsx
+++ src/containers/Instructions.jsx
@@ -5,6 +5,13 @@ const Instructions = (props) => {
   return (
     <div className="Instructions">
       <h2>We are in Instructions</h2>
+      <div className="summary">
+        <h2>{props.summaryTitle}</h2>
+        <img src={props.summaryPicUrl} />
+        <p>{props.summary}</p>
+      </div>
+      <br />
+      <Button href="/ingredients">Pick Ingredients</Button> <br />
       <Button href="/dashboard">Go to Dashboard</Button>
     </div>
   );
