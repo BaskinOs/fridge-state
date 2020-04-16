5c950baa8bfe83c7297c755dc867e124c7e363a4
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-16 02:09:19 -0700

    recipes component renders recipes dynamically

--- src/containers/Recipes.jsx
+++ src/containers/Recipes.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const Recipes = (props) => {
+  console.log('inside recipes list ', props.recipesList);
+  const recipes = props.recipesList.map((item, index) => {
+    return (
+      <div className="recipeCard">
+        <li>{item.name}</li>
+        <img
+          onClick={() => props.updateInstructions(item.id)}
+          src={item.picUrl}
+        ></img>
+      </div>
+    );
+  });
   return (
     <div className="Recipes">
       <h2>We are in Recipes</h2>
+      {recipes}
       <Button href="/instructions">Go to Instructions</Button>
       <br />
       <Button href="/dashboard">Back to Dashboard</Button>
